a4e8f272f2da0418b78199d52c459f857b1c237a
Stephen Hill <stephen.hill@mymail.champlain.edu>
2021-05-05 17:56:05 -0400

    Put in rest of functionality for the chase controller in c++. Works pretty well and also made the shooting ai have perception and use that to detect if player entered or exited range

--- Project3/Source/Project3/AIControllerCPP.cpp
+++ Project3/Source/Project3/AIControllerCPP.cpp
@@ -3,33 +3,81 @@
 
 #include "AIControllerCPP.h"
 #include "Perception/AIPerceptionComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 AAIControllerCPP::AAIControllerCPP()
 {
 	perceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
 	PrimaryActorTick.bCanEverTick = true;
+	moveToAcceptanceRadius = 50.f;
+}
+
+void AAIControllerCPP::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result)
+{
+	Super::OnMoveCompleted(RequestID, Result);
+	if (Result.Code == EPathFollowingResult::Success)
+	{
+		if (currentMoveType == MoveType::PLAYERFOLLOW)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, TEXT("Reached Target"));
+			UGameplayStatics::SpawnEmitterAtLocation(this, ExplosionTemplate, GetPawn()->GetActorLocation(),FRotator::ZeroRotator,GetPawn()->GetActorScale());//spawn particle system
+			TArray<AActor*> ignored;
+			UGameplayStatics::ApplyRadialDamage(this, explodeDamage, GetPawn()->GetActorLocation(), explodeRadius, UDamageType::StaticClass(), ignored, this, GetInstigatorController(), true);
+			if (GetPawn())
+			{
+				GetPawn()->Destroy();
+			}
+		}
+		else if (currentMoveType == MoveType::SELFMOVE)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, TEXT("Back to Original"));
+			GetPawn()->SetActorRotation(startingRotation);//goes back to starting rotation when reaching original position
+			//SetControlRotation(startingRotation);
+		}
+	}
+	/*else if(Result.Code == EPathFollowingResult::Blocked)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Emerald, Result.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Interupted"));
+		//StopMovement();
+		MoveToLocation(startingPosition, moveToAcceptanceRadius);//move back to starting location
+		currentMoveType = MoveType::SELFMOVE;//set correct type for checking when move is complete correctly
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("SELFMOVE"));
+	}*/
 }
 
 void AAIControllerCPP::BeginPlay()
 {
 	Super::BeginPlay();
+	//cast event for when ai percieves something
 	perceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &AAIControllerCPP::SensePlayer);
+	startingPosition = GetPawn()->GetActorLocation();//get start location
+	startingRotation = GetPawn()->GetActorRotation();//get start location
 }
 
 void AAIControllerCPP::SensePlayer(AActor* Actor, FAIStimulus Stimulus)
 {
 	if (Stimulus.WasSuccessfullySensed())//on sight
 	{
-		if (Actor->GetClass() == playerClass)
+		if (Actor->GetClass() == playerClass)//check its the player
 		{
+			playerObject = Actor;
+			MoveToActor(playerObject, moveToAcceptanceRadius);//start moving toward player
+			currentMoveType = MoveType::PLAYERFOLLOW; //set correct move type for when its complete
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("PLAYERFOLLOW"));
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Detected"));
 		}
 	}
-	else
+	else //on lose sight
 	{
-		if (Actor->GetClass() == playerClass)
+		if (Actor->GetClass() == playerClass)//check its the player
 		{
+			playerObject = nullptr;
+			//StopMovement();//stop moving if player out of range
+			MoveToLocation(startingPosition, -1);//move back to starting location
+			//if(res == EPathFollowingRequestResult::RequestSuccessful)
+			currentMoveType = MoveType::SELFMOVE; //set correct move type for when its complete
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Left Sight"));
 		}
 	}
--- Project3/Source/Project3/AIControllerCPP.h
+++ Project3/Source/Project3/AIControllerCPP.h
@@ -15,6 +15,12 @@ UCLASS()
 class PROJECT3_API AAIControllerCPP : public AAIController
 {
 	GENERATED_BODY()
+
+	enum MoveType
+	{
+		PLAYERFOLLOW,
+		SELFMOVE
+	};
 protected:
 	UPROPERTY(VisibleAnywhere, Category = "AI")
 	UAIPerceptionComponent* perceptionComp;
@@ -22,10 +28,33 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Setup")
 	TSubclassOf<ACharacter> playerClass;
 
+	UPROPERTY(BlueprintReadOnly,Category = "Setup")
+	AActor* playerObject;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	UParticleSystem* ExplosionTemplate;
+
+	FVector startingPosition;
+	FRotator startingRotation;
+	MoveType currentMoveType;
+
 	virtual void BeginPlay() override;
 
 	UFUNCTION()
 	void SensePlayer(AActor* Actor, FAIStimulus Stimulus);
 public:
 	AAIControllerCPP();
+
+	//When movement is completed this is automatically called
+	void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	float moveToAcceptanceRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float explodeDamage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float explodeRadius;
+
 };
