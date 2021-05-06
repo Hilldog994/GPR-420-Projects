72cc8c4f5e9a577d11e42c29896640a9029acef5
Cameron Murphy <cmurph2412@gmail.com>
2021-05-06 16:06:21 -0400

    Made some smaller changes to the level and UI

--- Project3/Source/Project3/FunctionLibrary.cpp
+++ Project3/Source/Project3/FunctionLibrary.cpp
@@ -2,18 +2,20 @@
 
 
 #include "FunctionLibrary.h"
-//#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 #include "Kismet/GameplayStatics.h"
 
+//Returns true if all enemies in the room are killed. Returns false if there are still some left
 bool UFunctionLibrary::DetermineIfEnemiesKilled(AActor* actor, TSubclassOf<AActor> actorClass)
 {
-	UWorld* world = actor->GetWorld();
+	//Determines world based on given actor
+	UWorld* world = actor->GetWorld(); 
 
+	//Stores all enemies in an array
 	TArray<AActor*> FoundActors;
 	UGameplayStatics::GetAllActorsOfClass(world, actorClass, FoundActors);
 
+	//Determines length of array. If 0 (no enemies found) returns true
 	int numEnemies = FoundActors.Num();
-
 	if (numEnemies <= 0)
 	{
 		return true;
@@ -22,6 +24,4 @@ bool UFunctionLibrary::DetermineIfEnemiesKilled(AActor* actor, TSubclassOf<AActo
 	{
 		return false;
 	}
-
-	return false;
 }
